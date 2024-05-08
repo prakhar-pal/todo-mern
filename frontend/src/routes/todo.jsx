47efb003744ec20d8de5b625ba86dc15962c27d6
Prakhar Pal <prakharpalpfdr+github@gmail.com>
2024-05-08 20:43:35 +0530

    feat: update todo data after updating the tood

--- frontend/src/routes/todo.jsx
+++ frontend/src/routes/todo.jsx
@@ -2,21 +2,27 @@ import React, { useState} from "react";
 import todoApiService from "../services/apis/todo.service";
 import { Link, useNavigate } from "react-router-dom";
 import { Space, Button } from "antd";
-import { useLoaderData } from "react-router";
+import { useLoaderData, useRevalidator } from "react-router";
 import TodoCard from "../components/TodoCard";
 import UpdateTodoModal from "../components/UpdateTodoModal.jsx";
 
 function EditTodo() {
     const { todo } = useLoaderData();
+    const revalidator = useRevalidator();
     const [showUpdateTodoModal, setShowUpdateTodoModal] = useState(false);
     const navigate = useNavigate();
     function handleGoBack(e) {
         e.preventDefault();
         navigate("/");
     }
+
+    function onCloseUpdateTodoModal () {
+        setShowUpdateTodoModal(false);
+        revalidator.revalidate();
+    }
     return (
         <>
-            {showUpdateTodoModal && <UpdateTodoModal {...todo} onClose={() => setShowUpdateTodoModal(false)}/>}
+            {showUpdateTodoModal && <UpdateTodoModal {...todo} onClose={onCloseUpdateTodoModal}/>}
             <Link className="flex mt-4" onClick={handleGoBack}>
                 <span>&larr;</span>
                 <h3>Details</h3>
