19ef55335fe2057fe63b09b5fea3541037f1d13a
Prakhar Pal <prakharpalpfdr+github@gmail.com>
2024-05-10 18:21:36 +0530

    feat: added color coding for todo cards

--- frontend/src/components/TodoCard.jsx
+++ frontend/src/components/TodoCard.jsx
@@ -12,15 +12,24 @@ const TodoCard = ({ title, description, status, className, _id, fullDescription 
         return option ?? TodoStatusOptions[0];
     }, [status])
     return (
-        <div className={clsx('rounded bg-white px-4 py-2 cursor-pointer relative', className, {'h-24': !fullDescription})} role="button" onClick={openTodoDetailsPage}>
+        <div
+            className={clsx('rounded px-4 py-2 cursor-pointer relative', className, {
+                'h-24': !fullDescription,
+                "bg-purple-400": statusObject.value == "TODO",
+                "bg-green-600 text-white": statusObject.value == "DONE",
+                "bg-green-400 text-black": statusObject.value == "IN_PROGRESS",
+
+            })}
+            role="button"
+            onClick={openTodoDetailsPage}>
             <h4 className="font-semibold">{title} </h4>
             <div className={clsx("font-light", !fullDescription ? "h-12 overflow-hidden" : "")}>
                 {description}
             </div>
             <div className={clsx("absolute right-2 bottom-2 h-4 italic text-xs border rounded border-gray-300 px-3 py-2 flex items-center justify-center", {
-                "bg-yellow-300": statusObject.value == "TODO",
-                "bg-green-600 text-white": statusObject.value == "DONE",
-                "bg-purple-500 text-white": statusObject.value == "IN_PROGRESS",
+                "text-white": statusObject.value == "TODO",
+                "text-white": statusObject.value == "DONE",
+                "text-black": statusObject.value == "IN_PROGRESS",
             })}>
                 {statusObject.label}
             </div>
